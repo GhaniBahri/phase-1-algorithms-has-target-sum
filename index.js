899142371bd2881ec12b2ab5f8f1f6e9c87dd532
GhaniBahri <bahri.ghanou@gmail.com>
2023-08-31 19:12:17 +0200

    Completed Assignment

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+//  for (let i=0; i<array.length; i++){
+//   for (let j=i+1; j<array.length; j++){
+//     const sum = array[i]+ array[j]
+//     if(sum  === target){
+//       return true
+//     }
+//   }
+//  }
+
+let found = false
+  array.forEach((element, id) => { 
+        for (let index = id+1; index < array.length; index++){
+          const sum = element + array[index];
+          if(sum  === target){
+            found = true
+            return
+          }
+       
+        }
+  }
+  );
+  return found
 }
+
+/* --------- another version with loop inside loop -------- */
+function hasTargetSum2(array, target) {
+ for (let i=0; i<array.length; i++){
+  for (let j=i+1; j<array.length; j++){
+    const sum = array[i]+ array[j]
+    if(sum  === target){
+      return true
+    }
+  }
+ }
+return false
+}
+
 
 /* 
   Write the Big O time complexity of your function here
